94988b633b269e3e3c04758213fb702816c97d02
Guillaume SALVA <gsalva@theroom.com>
2023-03-10 13:40:06 -0800

    Update 3-writeline.cs

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 
+namespace _3_writeline
+{
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("\"Programming is like building a multilingual puzzle");
     }
+}
 }
